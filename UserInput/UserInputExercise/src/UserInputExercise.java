5bed17561969a98e18c7ffaa3a9cb3746baedbf9
Emer Rocha <emerrocha@outlook.com>
2023-04-01 14:59:12 -0600

    Changes to UserInputSolution and UserInputExercise to be clearer, and to adhere to the BeginnerJava user guide

--- UserInput/UserInputExercise/src/UserInputExercise.java
+++ UserInput/UserInputExercise/src/UserInputExercise.java
@@ -3,16 +3,30 @@
 public class UserInputExercise {
     public static void main(String[] args) {
 
-        //STEP 2: Declare a new scanner class
+        /* STEP 1: Declare a new scanner class */
         //ADD SCANNER CLASS NAMED <scanner> HERE
 
 
-        //STEP 3: Declare a variable called <input> that is equal to the scanner
+        /* STEP 2: Declare a variable called <input> that is equal to the scanner */
+
+        System.out.println("Say whatever you want and I will display it to the console: ");
+
         //ADD A VARIABLE NAMED <input> OF TYPE STRING AND SET TO SCANNER CLASS HERE
 
+        /* STEP 3: Print your <input> variable to the console */
 
-        //STEP 4: Print your <input> variable to the console
-        System.out.println(input);
 
+
+        /* STEP 4: Capture a users numerical input */
+
+        //ADD A {System.out.println} STATEMENT ASKING USER TO INPUT THEIR NUMBER
+        //ADD A VARIABLE <integerInput> AND SET IT EQUAL TO YOUR {scanner.NextInt()} TO GRAB THE INT
+
+        /* STEP 5: Use the modulo operator to see if the number is odd or even */
+
+        //ADD A <oddOrEven> VARIABLE OF TYPE BOOLEAN
+        //COMPUTE <oddOrEven> MODULO 2 AND USING EQUALITY OPERATOR DETERMINE IF THAT NUMBER IS ODD OR EVEN
+
+        // STEP 6: Print out whether your number is odd or even
     }
 }
--- UserInput/UserInputSolution/src/UserInputSolution.java
+++ UserInput/UserInputSolution/src/UserInputSolution.java
@@ -3,16 +3,32 @@ import java.util.Scanner;
  public class UserInputSolution {
     public static void main(String[] args) {
 
-        // STEP 2: Declare a new scanner class
-        // ADD SCANNER CLASS NAMED <scanner> HERE
-        Scanner scanner = new Scanner(System.in);
+        /* STEP 1: Declare a new scanner class */
 
-        // STEP 3: Declare a variable called <input> that is equal to the scanner
-        // ADD A VARIABLE NAMED <input> OF TYPE STRING AND SET TO SCANNER CLASS HERE
-        String input = scanner.nextLine();
+        Scanner scanner = new Scanner(System.in);// ADD SCANNER CLASS NAMED <scanner> HERE
 
-        // STEP 4: Print your <input> variable to the console
+        /* STEP 2: Declare a variable called <input> that is equal to the scanner */
+
+        System.out.println("Say whatever you want and I will display it to the console: ");
+
+        String input = scanner.nextLine(); // ADD A VARIABLE NAMED <input> OF TYPE STRING AND SET TO SCANNER CLASS HERE
+
+        /* STEP 3: Print your <input> variable to the console */
         System.out.println(input);
+
+        /* STEP 4: Capture a users numerical input */
+
+        System.out.println("Enter a number: "); //ADD A {System.out.println} STATEMENT ASKING USER TO INPUT THEIR NUMBER
+        int integerInput = scanner.nextInt(); //ADD A VARIABLE <integerInput> AND SET IT EQUAL TO YOUR {scanner.NextInt()} TO GRAB THE INT
+
+        /* STEP 5: Use the modulo operator to, so you can see if the number is odd or even */
+
+        boolean oddOrEven; //ADD A <oddOrEven> VARIABLE OF TYPE BOOLEAN
+        oddOrEven = ((integerInput % 2) == 0); //COMPUTE <oddOrEven> MODULO 2 AND USING EQUALITY OPERATOR DETERMINE IF THAT NUMBER IS ODD OR EVEN
+
+        /* STEP 6: Print out whether your number is odd or even */
+
+        System.out.println("Your number is even is this true or false? " + oddOrEven);
 
     }
 
