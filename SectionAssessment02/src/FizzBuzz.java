a7d2b87361af999e477b67a7b5f0f51160175013
Emer Rocha <emerrocha@outlook.com>
2023-04-03 18:15:01 -0600

    Added some templates of other projects that will be worked.

---
+++ SectionAssessment02/src/FizzBuzz.java
@@ -0,0 +1,14 @@
+//*******************************************
+//*
+//* It's FizzBuzzing time! One of the most common introductory programming practices is the FIZZBUZZ problem
+//* The explanation is easy each time that any item is divisible by 5 print FIZZ, if it's divisible by 3 print BUZZ
+//* If the number is divisible by both 5 and 3 print FIZZBUZZ
+//*******************************************
+
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+
+
+    }
+}
