6aad91168c120fb7767e67a51910a26415e997d5
Emer-Rocha <103082600+Emer-Rocha@users.noreply.github.com>
2023-04-09 13:10:39 -0600

    Added a header file that will be standard for all of the projects, changed some configurations

---
+++ TypeCasting/TypeCastingExercise/src/TypeCastingExercise.java
@@ -0,0 +1,20 @@
+//***************************************************************************
+//*                      Type Casting
+//*
+//*             April, 9th 2023 	HelloWorldOfficial
+//*
+//*     Description: This exercise covers how to change the type of numerical variables
+//*     These principals will become important as we get into doing calculations with variables, and correctly receiving
+//*     the desired output.
+//*
+//*
+//*
+//*
+//*
+//***************************************************************************
+
+public class TypeCastingExercise {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
