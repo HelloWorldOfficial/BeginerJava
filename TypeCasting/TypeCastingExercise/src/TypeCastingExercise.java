e1693b212cc5c9277213be95520b13c137644cf9
Emer-Rocha <103082600+Emer-Rocha@users.noreply.github.com>
2023-04-10 21:40:18 -0600

    Added the data casting solution and exercise

--- TypeCasting/TypeCastingExercise/src/TypeCastingExercise.java
+++ TypeCasting/TypeCastingExercise/src/TypeCastingExercise.java
@@ -15,6 +15,26 @@
 
 public class TypeCastingExercise {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        byte smallest = 10; // LEAVE THIS
+        int small = 8; // LEAVE THIS
+
+        /* Step 1: Practice changing a "small" data type into a "bigger" data type
+         * Ths is what is known as Widening Primitive Conversion */
+        System.out.println();
+
+        /* Step 2: You may have noticed that this is not evaluating to what you would expect
+         this is an example of a logical error lets try and fix this by declaring a variable <decimal> with a double type */
+
+
+       // This is an example of widening primitive as you were able to print out the correct value of the decimal by converting
+        // your "small" data type into a bigger data type
+
+        double big = 99.8;
+
+        /*Step 3: We are now going to practice Narrowing Primitive Conversion
+         * We will do this by creating a simple algorithm that shows us the closets round number */
+        System.out.println();
+
     }
 }
---
+++ TypeCasting/TypeCastingSolution/src/TypeCastingSolution.java
@@ -0,0 +1,30 @@
+public class TypeCastingSolution {
+
+    public static void main(String[] args) {
+
+        /* Step 1: Practice changing a "small" data type into a "bigger" data type
+         * This is what is known as Widening Primitive Conversion for this exercise try and change the value of the 0 in <small>
+           to a number with a decimal (ex: 12.8, 23.5,0.8)*/
+
+        byte smallest = 10; // LEAVE THIS
+        int small = (smallest * 0); // LEAVE THIS
+
+
+        /* Step 2: You may have noticed that there is an error demonstrating that says "required type int, provided type double"
+         this is an example of a type error lets try and fix this by declaring a variable <decimal> with a double type
+         and setting that to the value */
+
+        double decimal = (smallest * 0);
+        System.out.println(decimal);
+
+        // This is an example of widening primitive as you were able to print out the correct value of the decimal by converting
+        // your "small" data type into a bigger data type
+
+        double big = 99.8;
+
+        /*Step 3: We are now going to practice Narrowing Primitive Conversion
+         * We will do this by creating a simple algorithm that shows us the closets round number */
+        System.out.println((int)big);
+
+    }
+}
