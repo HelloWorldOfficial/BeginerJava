5011672b9234efeb20b20d03b5ac20e790618a0d
Emer Rocha <emerrocha@outlook.com>
2023-04-17 15:58:52 -0600

    Started working on solution for Section assesment

---
+++ Section01/SectionAssessment01/src/SectionAssessmentSolution.java
@@ -0,0 +1,81 @@
+//***************************************************************************
+//*
+//* 					Section Assessment 01
+//*
+//* 			TBD            HelloWorldOfficial
+//*
+//* Description: This project is going to test the knowledge that you have learned so far. You will be tested on what you have learnt in this section
+//*              For this project you will be writing a program that displays a customer their water bill. Please refer to the section 01 assessment document that
+//*              is within this directory to figure out requirements. Remember to have fun! If you get stuck look through the solutions exercise
+//* Edited: TBD    Edited:HelloWorldOfficial     Version:1
+//*
+//*
+//*
+//***************************************************************************
+import java.util.Scanner;
+
+public class SectionAssessmentSolution {
+
+    public static void main(String[] args) {
+          /* Step 1: Declare your variables for this project. Remember to look at the pdf document to figure out what variables you will need. The base cost of electricity for a
+        civilian and enterprise customer are already declared
+         */
+
+        final double baseCostCivilian = 100;
+        final double baseCostEnterprise = 300;
+
+        String typeOfCustomerString;
+        int typeOfCustomer;
+        double totalCost;
+        double hoursUsed;
+        double hoursUsedConversion;
+        double civilianRate;
+        double enterpriseRate;
+
+        /* Step 2: Grab the customers info and whether they are a business or civilian customer, also grab how many hours they used electricity for.  */
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to Electricity Bill calculator !");
+        System.out.println("Please type a 1 if you are a Civilian Customer, and a 2 if you are an enterprise customer");
+
+        typeOfCustomerString = scanner.nextLine();
+        typeOfCustomer = Integer.parseInt(typeOfCustomerString);
+
+        if (typeOfCustomer < 1 || typeOfCustomer > 2)
+        {
+            System.out.println("Sorry this is not a valid type of customer, close the program out and try again. Goodbye!");
+            System.exit(0);
+        }
+        //REMEMBER TO CONVERT THAT String DATA INTO A USABLE NUMERICAL VALUE AND TO EVALUATE THAT THE USER HAS NOT PUT IN A UNUSABLE VALUE IN FOR THEIR ANSWER
+
+        /* Step 3: Declare your if branch to determine whether the customer is a civilian customer or if they are a business customer */
+
+        if (typeOfCustomer == 1)
+        {
+
+
+
+        }
+        else
+        {
+
+
+        }
+
+
+        /* Step 5A: calculate the monthly cost of electricity for a civilian customer with the number of hours that was provided, and the rate given in the pdf*/
+
+        /* Step 5B: calculate the monthly cost of electricity for an enterprise customer with the number of hours that the customer provided, and the rate given in the pdf*/
+
+        /* Step 6: Ask the customer what they budgeted to spend and capture that into a variable, afterwards determine whether the customer over spent or came in below what they budgeted.
+        * If the civilian customer spent less than $150 or budgeted to only spend $170 give them a 10% discount on their total price.
+        * If the enterprise customer spent less than $400 or budgeted to only spend $430 give them a 5% discount on their total price. */
+
+        /* Step 7: Display the total price to the customer that they will spend, as well as how much variance there is between their budgeted price and their total price, and also whether you
+        * gave them a discount or not.*/
+
+        //ALL MONETARY VALUES SHOULD BE WRITTEN TO ONLY HAVE 2 DECIMAL PLACES EX:{$500.70}
+
+        /* After you have completed this assignment and you are happy the results please open up the Assessment 01 after report page */
+    }
+}
